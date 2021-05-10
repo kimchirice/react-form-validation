46d4729d30f95e8edcaee1b8068bdb03d226ae02
Max Du <kimchi@u12.co>
2021-05-10 13:54:15 +1000

    [Add] form validation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,200 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useReducer } from "react"
+import { UPDATE_FORM, onInputChange, onFocusOut, validateInput } from "./utils/formUtils"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+/**
+ * The initial state of the form
+ * value: stores the value of the input field
+ * touched: indicates whether the user has tried to input anything in the field
+ * hasError: determines whether the field has error.
+ *           Defaulted to true since all fields are mandatory and are empty on page load.
+ * error: stores the error message
+ * isFormValid: Stores the validity of the form at any given time.
+ */
+ const initialState = {
+  name: { value: "", touched: false, hasError: true, error: "" },
+  email: { value: "", touched: false, hasError: true, error: "" },
+  password: { value: "", touched: false, hasError: true, error: "" },
+  mobile: { value: "", touched: false, hasError: true, error: "" },
+  terms: { value: false, touched: false, hasError: true, error: "" },
+  isFormValid: false,
 }
 
-export default App;
+/**
+ * Reducer which will perform form state update
+ */
+const formsReducer = (state, action) => {
+  switch(action.type) {
+    case UPDATE_FORM:
+      const {name, value, touched, hasError, error, isFormValid} = action.data
+      return {
+        ...state,
+        [name]: {
+          ...state[name],
+          value,
+          touched,
+          hasError,
+          error
+        },
+        isFormValid
+      }
+    default:
+      return state
+  }
+}
+
+
+
+function App() {
+  const [formState, dispatch] = useReducer( formsReducer, initialState )
+  
+  const [showError, setShowError] = useState(false)
+
+  const formSubmitHandler = event => {
+    event.preventDefault()
+
+    let isFormValid =  true
+
+    for ( const name in formState ) {
+      const { value } = formState[name]
+
+      const { hasError, error } = validateInput(name, value)
+
+      if (hasError) {
+        isFormValid = false
+      }
+      
+      if (name) {
+        dispatch({
+          type: UPDATE_FORM,
+          data: {
+            name,
+            value,
+            hasError,
+            error,
+            touched: true,
+            isFormValid,
+          },
+        })
+      } 
+    }
+
+    if (!isFormValid) {
+      setShowError(true)
+    } else {
+      //logic to submit the form data to backend
+      console.table(formState)
+      setTimeout(()=> window.alert('submit your info', formState), 2000 )
+      
+    }
+
+    setTimeout(() => {
+      setShowError(false)
+    }, 5000 )
+  }  
+
+  return (
+    <div className="App">
+      <h1 className="title">Sign Up</h1>
+      { showError && !formState.isFormValid && (
+        <div className = "form_error">Please fill all the fields correctly</div>
+      )}
+      <form onSubmit={ formSubmitHandler }>
+        <div className="input_wrapper">
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            value={formState.name.value}
+            onChange={e => {
+              onInputChange("name", e.target.value, dispatch, formState)
+            }}
+            onBlur={e => {
+              onFocusOut("name", e.target.value, dispatch, formState)
+            }}
+          />
+          {formState.name.touched && formState.name.hasError && (
+            <div className="error">{formState.name.error}</div>
+          )}
+        </div>
+        <div className="input_wrapper">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={formState.email.value}
+            onChange={e => {
+              onInputChange("email", e.target.value, dispatch, formState)
+            }}
+            onBlur={e => {
+              onFocusOut("email", e.target.value, dispatch, formState)
+            }}
+          />
+          {formState.email.touched && formState.email.hasError && (
+            <div className="error">{formState.email.error}</div>
+          )}
+        </div>
+        <div className="input_wrapper">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={formState.password.value}
+            onChange={e => {
+              onInputChange("password", e.target.value, dispatch, formState)
+            }}
+            onBlur={e => {
+              onFocusOut("password", e.target.value, dispatch, formState)
+            }}
+          />
+          {formState.password.touched && formState.password.hasError && (
+            <div className="error">{formState.password.error}</div>
+          )}
+        </div>
+        <div className="input_wrapper">
+          <label htmlFor="mobile">Mobile:</label>
+          <input
+            type="text"
+            name="mobile"
+            id="mobile"
+            value={formState.mobile.value}
+            onChange={e => {
+              onInputChange("mobile", e.target.value, dispatch, formState)
+            }}
+            onBlur={e => {
+              onFocusOut("mobile", e.target.value, dispatch, formState)
+            }}
+          />
+          {formState.mobile.touched && formState.mobile.hasError && (
+            <div className="error">{formState.mobile.error}</div>
+          )}
+        </div>
+        <div className="input_wrapper">
+          <label className="toc">
+            <input
+              type="checkbox"
+              name="terms"
+              checked={formState.terms.value}
+              onChange={e => {
+                onFocusOut("terms", e.target.checked, dispatch, formState)
+              }}
+            />
+            Accept terms and conditions
+          </label>
+          {formState.terms.touched && formState.terms.hasError && (
+            <div className="error">{formState.terms.error}</div>
+          )}
+        </div>
+        <div className="input_wrapper">
+          <input className="submit_btn" type="submit" value="Sign Up"
+          />
+        </div>
+      </form>
+    </div>
+  )
+}
+export default App
